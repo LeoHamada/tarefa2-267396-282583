dc19bed5989e1cbd75af47771ce6acde1990b6a1
LeoHamada <81055164+LeoHamada@users.noreply.github.com>
2026-09-14 15:43:32 -0300

    Add files via upload

---
+++ placaFaceDupla.java
@@ -0,0 +1,23 @@
+public class placaFaceDupla extends placaCircuito {
+    private static final double QUALIDADE = 0.7;
+    private static final double COBRE_POR_UNIDADE = 70.0; // cm²
+
+    public placaDuplaFace(String nome) {
+        super(nome, COBRE_POR_UNIDADE, QUALIDADE);
+    }
+
+    @Override
+    public void processar() {
+        System.out.println("Gravando trilhas nas duas faces de " + getNome() + "...");
+    }
+
+    @Override
+    public int calcularTempoProducao() {
+        return 25;
+    }
+
+    @Override
+    public String getTipo() {
+        return "Dupla Face";
+    }
+}
